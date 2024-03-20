db0b77981493cfe784e90d0ed866487cbc4a9909
shuiruge <shuiruge@whu.edu.cn>
2024-03-20 10:28:57 +0800

    Give a better function name.

--- actions/utils.py
+++ actions/utils.py
@@ -1,11 +1,19 @@
 import tensorflow as tf
 
 
-def get_gradient_loss_fn(get_loss):
-    def get_gradient_loss(inputs):
-        with tf.GradientTape() as tape:
-            tape.watch(inputs)
-            loss = get_loss(inputs)
-        loss_grads = tape.gradient(loss, inputs)
-        return sum(tf.reduce_mean(tf.square(x)) for x in loss_grads)
-    return get_gradient_loss
+def get_gradient_loss_fn(loss_fn):
+  """
+  Args:
+    loss_fn: Callable
+      Accepts a tensor or a list of tensors, and returns a scalar.
+  
+  Returns: Callable
+    The same signature as get_loss.
+  """
+  def gradient_loss_fn(inputs):
+    with tf.GradientTape() as tape:
+      tape.watch(inputs)
+      loss = loss_fn(inputs)
+    loss_grads = tape.gradient(loss, inputs)
+    return sum(tf.reduce_mean(tf.square(x)) for x in loss_grads)
+  return gradient_loss_fn
